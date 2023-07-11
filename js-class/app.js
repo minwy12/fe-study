c0a98caab4a2b19e9a57afed3b4d9d41a734fe6d
minwy12 <owen.min@daangn.com>
2023-07-11 20:13:19 +0900

    app.js

--- js-class/app.js
+++ js-class/app.js
@@ -1,1 +1,13 @@
-alert("test")
+const age_str = prompt("how old are you?");
+
+console.log("type of ", age_str, " is ", typeof(age_str))
+
+age_int = parseInt(age_str)
+
+console.log("type of ", age_int, " is ", typeof(age_int))
+
+if (isNaN(age_int)) {
+    console.log("NaN")
+} else {
+    console.log("not NaN")
+}
