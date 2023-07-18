c53e469742778eccb8448208ae70ed239f06ba16
minwy12 <owen.min@daangn.com>
2023-07-18 14:24:28 +0900

    add nodejs

---
+++ nodejs/calc.js
@@ -0,0 +1,10 @@
+function add(a, b) {
+  return a + b;
+}
+
+// console.log(add(1, 2));
+
+module.exports = {
+  moduleName: "calc module",
+  add: add,
+};
---
+++ nodejs/index.js
@@ -0,0 +1,6 @@
+const calc = require("./calc");
+console.log(calc);
+
+console.log("hello VSCode!");
+
+console.log(calc.add(1, 2));
